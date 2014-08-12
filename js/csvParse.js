fdf9cd6339d068ad66ba496395f89d2e63789eb5
JessicaBarnett <BarnettEffect@gmail.com>
2014-08-11 20:46:10 -0400

    commented out some in-progress stuff to make this code testable

--- js/csvParse.js
+++ js/csvParse.js
@@ -1,4 +1,62 @@
+//buckets for normalized and nested object lists
 var normal, nested;
+
+//vars for nested csv parser
+var tree;
+var keys = {
+    0: {
+        "name": "FUNCTION_CLASS_NAME",
+        "code": "FUNCTION_CLASS"
+    },
+    1: {
+        "name": "FUNCTION_GROUP_NAME",
+        "code": "FUNCTION_GROUP"
+    },
+    2: {
+        "name": "FUNCTION_NAME",
+        "code": "FUNCTION"
+    },
+    3: {
+        "name": "ACTIVITY_NAME",
+        "code": "ACTIVITY_CODE"
+    }
+};
+
+//utility for nested csv parser
+function makeNode(level, d) {
+    var newNode;
+
+    var name = keys[level]["name"];
+    var code = keys[level]["code"];
+
+    newNode = {
+        "name": d[name],
+        "code": d[code]
+    }
+
+    if (level < 3)
+        newNode.children = {}; //need an array here eventually
+    else{
+             newNode["current"] = {
+                "operating": +d.OPERATING_CYEST_LUMPSUM_AMT,
+                "grant": +d.GRANT_CYEST_LUMPSUM_AMT,
+                "capital": +d.CAPITAL_CYEST_LUMPSUM_AMT,
+                "other": +d.OTHER_CYEST_LUMPSUM_AMT,
+                "total": +d.CYEST_LUMPSUM_TOT
+            }
+
+            newNode["next"] = {
+                "operating": +d.OPERATING_ACT_LUMPSUM_AMT,
+                "grant": +d.GRANT_ACT_LUMPSUM_AMT,
+                "capital": +d.CAPITAL_ACT_LUMPSUM_AMT,
+                "other": +d.OTHER_ACT_LUMPSUM_AMT,
+                "total": +d.ACT_LUMPSUM_TOT
+                }
+    }
+
+    return newNode;
+}
+
 
 /**************   Normalized CSV Format   ***************/
 
@@ -51,28 +109,8 @@ function makeNormalizedList(index, d) {
 
 normal = parseNormalizedCSV();
 
+
 /**************   Nested CSV Format   ***************/
-
-
-var tree;
-var keys = {
-    0: {
-        "name": "FUNCTION_CLASS_NAME",
-        "code": "FUNCTION_CLASS"
-    },
-    1: {
-        "name": "FUNCTION_GROUP_NAME",
-        "code": "FUNCTION_GROUP"
-    },
-    2: {
-        "name": "FUNCTION_NAME",
-        "code": "FUNCTION"
-    },
-    3: {
-        "name": "ACTIVITY_NAME",
-        "code": "ACTIVITY_CODE"
-    }
-};
 
 
 function parseNestedCSV() {
@@ -137,7 +175,6 @@ function parseNestedCSV() {
                 tree["children"][name00]["children"][name01]["children"][name02]["children"][name03] = makeNode(i, d); 
             }
 
-
         },
         //callback.  Actions to take after csv file has been fully parsed
         function(dataArray) {
@@ -150,159 +187,47 @@ function parseNestedCSV() {
 
 nested = parseNestedCSV();
 
-/**** RECURSIVE NOT-SO-FUNCTIONAL SOLUTION  ****/
+// function nestedChildrenToArrays(tree){
+//     var key;
+//     var nameKey00, nameKey01, nameKey02, nameKey03;
+//     var name00, name01, name02, name03;
+//     var i = 0;
+//     name00 = d[keys[i]["name"]];
+//     i++;
+//     name01 = d[keys[i]["name"]];
+//     i++;
+//     name02 = d[keys[i]["name"]];
+//     i++;
+//     name03 = d[keys[i]["name"]];
 
+//     //go through all properties in object named children
 
-function parseNestedCSVrecursive() {
-    var debugIndex = 0;
-    tree = {
-        "name": "School District of Philadelphia Budget",
-        "yearCurrent": 2014,
-        "yearNext": 2015,
-        "children": []
-    };
+//  for ("children" in tree)
+//     for (property in tree["children"])
 
-    return d3.csv("../data/test-budget-small.csv",
-        //accessor.  Controls how data is structured as it's pulled in
-        function(d) {
+//    tree["children"]
 
-            console.log("tree at row " + debugIndex);
-            debugIndex++;
+//    tree["children"][name00]["children"]
 
+//    tree["children"][name00]["children"][name01]["children"]
 
-            var newNode = growBranch(tree, 0, d); 
+//    tree["children"][name00]["children"][name01]["children"][name02]["children"]
 
-            //loops through all children of tree 
-            for(var i = 0; i < tree["children"].length; i++){
-                if(tree["children"][i]["name"] === d.FUNCTION_CLASS_NAME){ //finds child with current FUNCTION_CLASS_NAME
-                    tree["children"][i] = newNode;//replaces it with the new node which has the current row of data's changes added
-                    break; //ends the loop
-                }
-            }
-        },
-        //callback.  Actions to take after csv file has been fully parsed
-        function(dataArray) {
-            console.log(tree);
-        });
+//    tree["children"][name00]["children"][name01]["children"][name02]["children"][name03]["children"]
 
-}
+// }
 
+/**************   Adjustments    **************/
 
-//for each row in document, makeTree should be called 4 times, once for each key
-//returns the same parent node with any necessary changes made
-function growBranch(parent, level, d) {
-    var nameKey = keys[level]["name"];
-    var name = d[nameKey];
-    var siblingArray = parent["children"];
-    var nextLevelParent;
+// extract gap closing cuts and undistributed budgetary adjustments
 
-    //if there is not an element of this name in parent.children
-    if (!nodeExists(siblingArray, "name", name)) {
-        siblingArray.push(makeNode(level, d)); //access that element, add new element to its children array
-    }
-
-
-    if (level < 3) { //if we haven't hit bottom yet
-        //call makeTree for next level
-        //printNamesInArray(siblingArray);
-
-        nextLevelParent = getNodeFromArray(siblingArray, "name", name); // gets parent for next level
-        //siblingArray = siblingArray.children.push(growBranch(nextLevelParent, (level+1), d));
-        //parent["children"].push(growBranch(nextLevelParent, (level+1), d));
-
-        /***** have to replace node in siblingArray, not append to it *****/
-        //iterates through, finds right sibling, replaces
-        for (var i = 0; i < siblingArray.length; i++)
-            if (siblingArray[i]["name"] === name)
-                siblingArray[i] = growBranch(nextLevelParent, (level+1), d);
-    }
-    return parent;
-    //return siblingArray;
-
-}
-
-function printNamesInArray(theArray){
-    theArray.forEach(function(element, index, array) {
-        console.log(element.name);
-    });
-}
-
-
-function makeNode(level, d) {
-    var newNode;
-
-    var name = keys[level]["name"];
-    var code = keys[level]["code"];
-
-    newNode = {
-        "name": d[name],
-        "code": d[code]
-    }
-
-    if (level < 3)
-        newNode.children = {}; //need an array here eventually
-    else{
-             newNode["current"] = {
-                "operating": +d.OPERATING_CYEST_LUMPSUM_AMT,
-                "grant": +d.GRANT_CYEST_LUMPSUM_AMT,
-                "capital": +d.CAPITAL_CYEST_LUMPSUM_AMT,
-                "other": +d.OTHER_CYEST_LUMPSUM_AMT,
-                "total": +d.CYEST_LUMPSUM_TOT
-            }
-
-            newNode["next"] = {
-                "operating": +d.OPERATING_ACT_LUMPSUM_AMT,
-                "grant": +d.GRANT_ACT_LUMPSUM_AMT,
-                "capital": +d.CAPITAL_ACT_LUMPSUM_AMT,
-                "other": +d.OTHER_ACT_LUMPSUM_AMT,
-                "total": +d.ACT_LUMPSUM_TOT
-                }
-    }
-
-    return newNode;
-}
-
-//array is an array of objects
-//name is the name of the object we're looking for
-//returns true if it exists, false otherwise
-function nodeExists(array, property, value) {
-    if (typeof array == undefined || array.length < 1) //if array is undefined or empty
-        return false;
-
-    for (var i = 0; i < array.length; i++) {
-        if (array[i][property] === value ) {//if the element at array[i] contains a property that matches
-            return true;
-        }
-    }
-
-    return false;  //array exists but node not found
-}
-
-//returns node that contains the passed property
-function getNodeFromArray(array, property, value) {
-    if (!nodeExists(array, property, value)) //if array is undefined or empty, or doesn't contain property 
-        throw new Error("lookupNode: array undefined, empty, or property does not exist");
-
-    for (var i = 0; i < array.length; i++) {
-        if (array[i][property] == value) {
-            return array[i];
-        }
-    }
-
-    throw new Error("getNodeFromArray: match not found");
-    return null;
-}
-
-
-var testArray = [{
-    "name": "name1",
-    "s": "s",
-    "3": 0
-}, {
-    "l": "l",
-    "name": "alphabet"
-}, {
-    "q": "q",
-    "r": 10
-}];
+// var gapClosingAmounts = [{'Function' => 'F49992', 'ActivityCode' => '114A'}  // Budget Reductions - Instructional & Instructional Support
+//                         ,{'Function' => 'F49995', 'ActivityCode' => '114C'} // Budget Reductions - Operating Support
+//                         ,{'Function' => 'F49994', 'ActivityCode' => '114E'} // Budget Reductions - Administration
+//                         ,{'Function' => 'F49991', 'ActivityCode' => '114B'} // Budget Reductions - Pupil & Family Support
+//                         ,{'Function' => 'F41073', 'ActivityCode' => '5999'} // Undistributed Budgetary Adjustments - Other
+//                         ,{'Function' => 'F41073', 'ActivityCode' => '5221'} // Undistributed Budgetary Adjustments - Other
+//                         ,{'Function' => 'F41073', 'ActivityCode' => '5130'} // Undistributed Budgetary Adjustments - Other
+//                         ,{'Function' => 'F41073', 'ActivityCode' => '2817'} // Undistributed Budgetary Adjustments - Other
+//                 ];
 
