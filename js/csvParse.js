e0acf2262dc24f4a106c0ef8e14f846b91259577
JessicaBarnett <BarnettEffect@gmail.com>
2014-08-18 21:10:59 -0400

    functions to convert all objects stored in children properties is working

--- js/csvParse.js
+++ js/csvParse.js
@@ -1,7 +1,80 @@
 //buckets for normalized and nested object lists
 var normal, nested;
 
-//vars for nested csv parser
+/********************************************************/
+/**************   Normalized CSV Format   ***************/
+/********************************************************/
+
+
+function parseNormalizedCSV() {
+    var index = -1;
+
+    return d3.csv("../data/budget-information-test.csv",
+        function(d) {
+            index++; //I'm pretty sure there's a better way to do this...
+            return makeNormalizedListItem(index, d);
+        },
+        //callback.  Actions to take after csv file has been fully parsed
+        function(dataArray) {
+            //dataArray contains an array of objects, 1 for each row of the csv file
+            //object property names correspond to csv headings (FUNCTION, ACTIVITY_CODE, etc), 
+            //unless there's an accessor function involved
+            console.log("********   NORMALIZED   ********");
+            console.log(dataArray);
+            return(dataArray);
+        });
+}
+
+function makeNormalizedListItem(index, d) {
+    return {
+        "ID": index,
+        "Class": "NormalizedBudgetLine",
+        "Created": Date.now(), //value in budget-list-normalized: 1400189228  looked like a time to me but I think I'm wrong.  Too many digits 
+        "CreatorID": 1,
+        "FunctionClass": d.FUNCTION_CLASS,
+        "FunctionClassName": d.FUNCTION_CLASS_NAME,
+        "FunctionGroup": d.FUNCTION_GROUP,
+        "FunctionGroupName": d.FUNCTION_GROUP_NAME,
+        "Function": d.FUNCTION,
+        "FunctionName": d.FUNCTION_NAME,
+        "ActivityCode": d.ACTIVITY_CODE,
+        "ActivityName": d.ACTIVITY_NAME,
+        "CurrentOperating": +d.OPERATING_CYEST_LUMPSUM_AMT,
+        "CurrentGrant": +d.GRANT_CYEST_LUMPSUM_AMT,
+        "CurrentCapital": +d.CAPITAL_CYEST_LUMPSUM_AMT,
+        "CurrentOther": +d.OTHER_CYEST_LUMPSUM_AMT,
+        "CurrentTotal": +d.CYEST_LUMPSUM_TOT,
+        "ProposedOperating": +d.OPERATING_ACT_LUMPSUM_AMT,
+        "ProposedGrant": +d.GRANT_ACT_LUMPSUM_AMT,
+        "ProposedCapital": +d.CAPITAL_ACT_LUMPSUM_AMT,
+        "ProposedOther": +d.OTHER_ACT_LUMPSUM_AMT,
+        "ProposedTotal": +d.ACT_LUMPSUM_TOT,
+        "RunDate": d.RUN_DATE.replace(RegExp("/", "g"), "-")
+    };
+}
+
+normal = parseNormalizedCSV();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**********************************************/
+/************ Nested CSV Formatter ************/
+/**********************************************/
+
+/**************  nested tree variables  *************/
+
 var tree;
 var keys = {
     0: {
@@ -22,7 +95,10 @@ var keys = {
     }
 };
 
-//utility for nested csv parser
+//**************  nested tree utilities  ****************//
+
+
+//makes a new object out of passed datum and returns it
 function makeNode(level, d) {
     var newNode;
 
@@ -58,59 +134,31 @@ function makeNode(level, d) {
 }
 
 
-/**************   Normalized CSV Format   ***************/
+//iterates all objects in a nested tree
+//converts everything in a property named 
+//"children" from an object to an array
+function convertChildren(value, index, array){
+    //convert this element's children to an array
+    value["children"] = convertObjectToArray(value["children"]);
 
-
-function parseNormalizedCSV() {
-    var index = -1;
-
-    return d3.csv("../data/budget-information-test.csv",
-        function(d) {
-            index++; //I'm pretty sure there's a better way to do this...
-            return makeNormalizedList(index, d);
-        },
-        //callback.  Actions to take after csv file has been fully parsed
-        function(dataArray) {
-            //dataArray contains an array of objects, 1 for each row of the csv file
-            //object property names correspond to csv headings (FUNCTION, ACTIVITY_CODE, etc), 
-            //unless there's an accessor function involved
-            console.log("********   NORMALIZED   ********");
-            console.log(dataArray);
-        });
+    if(value["children"][0]["children"]){//if this element has children
+        //iterate through this element's children array; recursively call self each time
+        value["children"].forEach(convertChildren);
+    }
+    return;
 }
 
-function makeNormalizedList(index, d) {
-    return {
-        "ID": index,
-        "Class": "NormalizedBudgetLine",
-        "Created": Date.now(), //value in budget-list-normalized: 1400189228  looked like a time to me but I think I'm wrong.  Too many digits 
-        "CreatorID": 1,
-        "FunctionClass": d.FUNCTION_CLASS,
-        "FunctionClassName": d.FUNCTION_CLASS_NAME,
-        "FunctionGroup": d.FUNCTION_GROUP,
-        "FunctionGroupName": d.FUNCTION_GROUP_NAME,
-        "Function": d.FUNCTION,
-        "FunctionName": d.FUNCTION_NAME,
-        "ActivityCode": d.ACTIVITY_CODE,
-        "ActivityName": d.ACTIVITY_NAME,
-        "CurrentOperating": +d.OPERATING_CYEST_LUMPSUM_AMT,
-        "CurrentGrant": +d.GRANT_CYEST_LUMPSUM_AMT,
-        "CurrentCapital": +d.CAPITAL_CYEST_LUMPSUM_AMT,
-        "CurrentOther": +d.OTHER_CYEST_LUMPSUM_AMT,
-        "CurrentTotal": +d.CYEST_LUMPSUM_TOT,
-        "ProposedOperating": +d.OPERATING_ACT_LUMPSUM_AMT,
-        "ProposedGrant": +d.GRANT_ACT_LUMPSUM_AMT,
-        "ProposedCapital": +d.CAPITAL_ACT_LUMPSUM_AMT,
-        "ProposedOther": +d.OTHER_ACT_LUMPSUM_AMT,
-        "ProposedTotal": +d.ACT_LUMPSUM_TOT,
-        "RunDate": d.RUN_DATE.replace(RegExp("/", "g"), "-")
-    };
+//converts passed object into an array
+function convertObjectToArray(obj){
+    var arr = [];
+
+    for (property in obj){
+        arr.push(obj[property]);
+    }
+    return arr;
 }
 
-normal = parseNormalizedCSV();
-
-
-/**************   Nested CSV Format   ***************/
+/**************   Nested CSV Formater   ***************/
 
 
 function parseNestedCSV() {
@@ -161,7 +209,8 @@ function parseNestedCSV() {
 
             //if level 2 key node doesn't exist, add it
             if (!siblings02[name02]){
-                tree["children"][name00]["children"][name01]["children"][name02] = makeNode(i, d);             }
+                tree["children"][name00]["children"][name01]["children"][name02] = makeNode(i, d);
+             }
 
 
             i++;
@@ -178,44 +227,16 @@ function parseNestedCSV() {
         },
         //callback.  Actions to take after csv file has been fully parsed
         function(dataArray) {
+            convertChildren(tree, null, null);
+
             console.log("********  NESTED TREE  *******");
             console.log(tree);
 
+            return tree;
         });
-
 }
 
 nested = parseNestedCSV();
-
-// function nestedChildrenToArrays(tree){
-//     var key;
-//     var nameKey00, nameKey01, nameKey02, nameKey03;
-//     var name00, name01, name02, name03;
-//     var i = 0;
-//     name00 = d[keys[i]["name"]];
-//     i++;
-//     name01 = d[keys[i]["name"]];
-//     i++;
-//     name02 = d[keys[i]["name"]];
-//     i++;
-//     name03 = d[keys[i]["name"]];
-
-//     //go through all properties in object named children
-
-//  for ("children" in tree)
-//     for (property in tree["children"])
-
-//    tree["children"]
-
-//    tree["children"][name00]["children"]
-
-//    tree["children"][name00]["children"][name01]["children"]
-
-//    tree["children"][name00]["children"][name01]["children"][name02]["children"]
-
-//    tree["children"][name00]["children"][name01]["children"][name02]["children"][name03]["children"]
-
-// }
 
 /**************   Adjustments    **************/
 
